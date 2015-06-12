fb75472881f4f11146733aa84099314c2f08bfe0
David Ernst <git@dsernst.com>
2015-06-12 14:56:39 -0700

    solve 20 Factorial digit sum! thanks to big.js

--- 20 Factorial digit sum.js
+++ 20 Factorial digit sum.js
@@ -8,7 +8,7 @@
 
 function digitSum(number) {
   // converts number to string
-  var string = '' + number;
+  var string = "" + number;
   // splits string into array
   var array = string.split('');
   // reduces down array into sum
@@ -17,6 +17,37 @@ function digitSum(number) {
     return +val + memo;
   }, 0);
 }
+
+var Big = require('big.js');
+
+function bigFactorial(n) {
+  if (n === 1) {
+    return new Big(1);
+  }
+  return bigFactorial(n - 1).times(n);
+}
+
+// console.log(bigFactorial(10).toString()); // 3628800, it works!
+
+// console.log(bigFactorial(100).toString()); // 9.3326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864e+157
+// console.log(bigFactorial(100).toFixed()); // 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000
+console.log(digitSum(bigFactorial(100).toFixed())); // 648
+
+// Congratulations, the answer you gave to problem 20 is correct.
+
+// You are the 119305th person to have solved this problem.
+
+
+
+
+
+
+
+
+
+// ------------------------
+// Unsuccessful approaches:
+// ------------------------
 
 
 // looking for patterns where we might be able to reduce factors, like *10, or 2^4, or 3^4
@@ -71,16 +102,19 @@ var i;
 for (i = 1; i <= 24; i++) {
   // factorials[i] = factorial(i);
   factorialDigitSums[i] = digitSum(factorial(i));
-  console.log('(' + i + ', ' + digitSum(factorial(i)) + '), ');
+  // console.log('(' + i + ', ' + digitSum(factorial(i)) + '), ');
 }
 
 // console.log(factorials);
-console.log(factorialDigitSums);
+// console.log(factorialDigitSums);
 
 
 // // another approach:
 // var factorialsThatDidntAffectDigitSum =
 //   [4, 7, 8, 10, 13, 15, 17]
 
-console.log(factorial(21));
+// console.log(factorial(21));
 // before 21! digitSum'd to 63
+
+
+
