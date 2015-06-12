ee63d4c56ded92ba7427def8cee4037761b97232
David Ernst <git@dsernst.com>
2015-06-12 16:23:28 -0700

    solve 16 Power digit sum! with big.js

--- 16 Power digit sum.js
+++ 16 Power digit sum.js
@@ -2,20 +2,25 @@
 
 // What is the sum of the digits of the number 2^1000?
 
-var i;
-var result;
-var digitsSum;
-var resultString;
+var Big = require('big.js');
 
-var sumString = function (memo, num) {
-  return memo + parseInt(num, 10);
-};
+function digitSum(number) {
+  // converts number to string
+  var string = "" + number;
+  // splits string into array
+  var array = string.split('');
+  // reduces down array into sum
+  return array.reduce(function (memo, val) {
 
-for (i = 0; i < 100; i++) {
-  result = Math.pow(2, i);
-  digitsSum = result.toString().split('').reduce(sumString, 0);
-
-  if (i % 5 === 0) {
-    console.log(i, digitsSum);
-  }
+    return +val + memo;
+  }, 0);
 }
+
+var twoToThe1000 = new Big(2).pow(1000);
+console.log(twoToThe1000.toString()); // 1.0715086071862673209484250490600018105614048117055336074437503883703510511249361224931983788156958581275946729175531468251871452856923140435984577574698574803934567774824230985421074605062371141877954182153046474983581941267398767559165543946077062914571196477686542167660429831652624386837205668069376e+301
+console.log(digitSum(twoToThe1000.toFixed())); // 1366
+
+// Congratulations, the answer you gave to problem 16 is correct.
+
+// You are the 132599th person to have solved this problem.
+
