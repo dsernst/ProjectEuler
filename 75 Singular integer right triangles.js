b2ca343ba79a52e01641e2dce3c0e0fcbba7db72
David Ernst <git@dsernst.com>
2015-09-01 20:25:32 -0700

    second and third attempts at 75 Singular integer right triangles

--- 75 Singular integer right triangles.js
+++ 75 Singular integer right triangles.js
@@ -13,65 +13,55 @@
 
 // Given that L is the length of the wire, for how many values of L ≤ 1,500,000 can exactly one integer sided right angle triangle be formed?
 
-var allPythagorianTriples = [
-  [3, 4, 5],
-  [5, 12, 13],
-  [8, 15, 17],
-  [7, 24, 25],
-  [9, 40, 41],
-  [11, 60, 61],
-  [12, 35, 37],
-  [13, 84, 85],
-  [15, 112, 113],
-  [16, 63, 65],
-  [17, 144, 145],
-  [19, 180, 181],
-  [20, 21, 29],
-  [20, 99, 101],
-  [21, 220, 221],
-  [24, 143, 145],
-  [28, 45, 53],
-  [28, 195, 197],
-  [32, 255, 257],
-  [33, 56, 65],
-  [36, 77, 85],
-  [39, 80, 89],
-  [44, 117, 125],
-  [48, 55, 73],
-  [51, 140, 149],
-  [52, 165, 173],
-  [57, 176, 185],
-  [60, 91, 109],
-  [60, 221, 229],
-  [65, 72, 97],
-  [84, 187, 205],
-  [85, 132, 157],
-  [88, 105, 137],
-  [95, 168, 193],
-  [96, 247, 265],
-  [104, 153, 185],
-  [105, 208, 233],
-  [115, 252, 277],
-  [119, 120, 169],
-  [120, 209, 241],
-  [133, 156, 205],
-  [140, 171, 221],
-  [160, 231, 281],
-  [161, 240, 289],
-  [204, 253, 325],
-  [207, 224, 305],
-];
-
+var fs = require('fs');
 var _ = require('lodash');
 
-var sumOfTriples = allPythagorianTriples.map(_.sum);
+var target = 1500000;
 
-function isMultipleOfPythagorianTripleSum(number) {
-  return _.some(sumOfTriples, function (sum) {
-    return number % sum === 0;
-  });
+// var triples = require('pythagorean-triples');
+// var pythagorianTriples = triples.upToSum(target).filter(triples.isPrimitive);
+// fs.writeFileSync('./75 triples-up-to-' + target + '.json', JSON.stringify(pythagorianTriples));
+var pythagorianTriples = JSON.parse(fs.readFileSync('./75 triples-up-to-' + target + '.json'));
+
+var pythagorianTripleSums = pythagorianTriples.map(_.sum);
+
+// function isMultipleOfPythagorianTripleSum(number) {
+//   return _.some(pythagorianTripleSums, function (sum) {
+//     return number % sum === 0;
+//   });
+// }
+
+// function quickIsMultipleOfPythagorianTripleSum(number) {
+//   if (number % 5000 === 0) {
+//     console.log(number);
+//   }
+//   var isMultiple = false;
+//   _.each(pythagorianTripleSums, function (sum) {
+//     if (number % sum === 0) {
+//       isMultiple = true;
+//       return false;
+//     }
+//     if (sum > number) {
+//       return false;
+//     }
+//   });
+//   return isMultiple;
+// } // [Finished in 732.0s] for target = 1500000;
+
+var sieveOfMultiples = pythagorianTripleSums.reduce(function (sieve, sum) {
+  var i;
+  for (i = 1; i * sum <= sieve.length; i++) {
+    sieve[i * sum] = true;
+  }
+  return sieve;
+}, new Array(target + 2).join(',').split(',') // get values for map to work
+  .map(function () { return false; }));
+
+function quickerIsMultipleOfPythagorianTripleSum(number) {
+  return sieveOfMultiples[number];
 }
-// console.log([12, 24, 30, 36, 40, 48].map(isMultipleOfPythagorianTripleSum)); // [true, true, true, true, true, true]
-// console.log(isMultipleOfPythagorianTripleSum(20)); // false
 
-console.log(_.range(1, 1500001).filter(isMultipleOfPythagorianTripleSum).length); // 246337 [Finished in 1.7s]
+console.log([12, 24, 30, 36, 40, 48].map(quickerIsMultipleOfPythagorianTripleSum)); // [true, true, true, true, true, true]
+console.log(quickerIsMultipleOfPythagorianTripleSum(20)); // false
+
+console.log(_.range(1, target + 1).filter(quickerIsMultipleOfPythagorianTripleSum).length);
